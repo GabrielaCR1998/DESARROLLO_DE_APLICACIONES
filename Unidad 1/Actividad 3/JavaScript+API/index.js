55de1dab1c8ab4086f0d48dbe0b16c8b0acfad66
gabriela cruz <gabriela.cruz18@tectijuana.edu.mx>
2021-09-24 20:35:20 -0500

    practica 3 subida

--- Unidad 1/Actividad 3/JavaScript+API/index.js
+++ Unidad 1/Actividad 3/JavaScript+API/index.js
@@ -1,74 +1,27 @@
 function Leer() {
-    const peli = document.getElementById("input").value;
+    const ciudad = document.getElementById("input").value;
     //obtain an apikey on this web
     //http://www.omdbapi.com/apikey.aspx
-    const key='';
-    const api_url=`http://www.omdbapi.com/?s=${peli}&apikey=${key}`
-    buscar3(api_url);
+    const llave='437fda1a378e4de965b26c33814641ff';
+    const url=`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${llave}&units=metric`
+    buscar1(url);
 }
 
-function buscar1(api_url){
+function buscar1(url){
 
-    fetch(api_url)
+    fetch(url)
       .then(data => {
         return data.json()
       }).then(resultado=>{
             console.log(resultado);
 
-            const {Search=[]} = resultado;
+        
             
-            console.log(Search);
-            document.getElementById("lista").innerHTML='';
+            console.log(resultado.main)
 
-            Search.map((p)=>{
-                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
-                         <img width='100%' src=${p.Poster} alt="No hay poster"></img>
-            </div>`;
-            })
+        
+            document.getElementById("lista").innerHTML=`<h1>Temp: ${resultado.main.temp}</h1> <h2>Temp min: ${resultado.main.temp_min}</h2> <h3>Temp max: ${resultado.main.temp_max}</h3>`;        
       });
-
 
 }
 
-const buscar2=async(api_url)=>{
-
-    const data= await fetch(api_url);
-    const respuesta= await data.json();
-    const Search = await respuesta.Search;
-
-    console.log(Search);
-
-    if(Search!=null)
-    {   
-        document.getElementById("lista").innerHTML='';
-        Search.map((p)=>{
-                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
-                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
-        })
-
-    }
-
-}    
-
-     
-const buscar3=async(api_url)=>{
-
-    const respuesta= await axios(api_url);
-    const Search = await respuesta.data.Search;
-    console.log(respuesta.data);
-    
-    console.log(Search);
-
-    
-    if(Search!=null)
-    {
-        document.getElementById("lista").innerHTML='';
-        
-        Search.map((p)=>{
-                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
-                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
-        })
-
-    }
-
-}    
